6c934b1b7a71433c5a97a609af3697db704271fc
Jackson <porccode@gmail.com>
2026-09-04 11:28:12 -0300

    Estruturando o projeto de estudos do backend e Angular

---
+++ fundamentos/src/index.ts
@@ -0,0 +1,7 @@
+const nome: string = "Jackson";
+const curso: string = "Análise e Desenvolvimento de Sistemas";
+const estudando: boolean = true;
+
+console.log(nome);
+console.log(curso);
+console.log(estudando);

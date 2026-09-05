f940ee345565ef171f686806fa45f74d2ba7298d
Jackson <porccode@gmail.com>
2026-09-05 11:00:32 -0300

    Array e objetos juntos!

--- fundamentos/src/index.ts
+++ fundamentos/src/index.ts
@@ -1,2 +1,22 @@
-const prioridades: number[] = [1, 2, 3]
-console.log(prioridades);
+const tarefas = [
+   {
+      id: 1,
+      titulo: "Estudar TypeScript",
+      concluido: true,
+      prioridade: 1
+   },
+   {
+      id: 2,
+      titulo: "Estudar NestJS",
+      concluido: false,
+      prioridade: 2
+   },
+   {
+      id: 3,
+      titulo: "Esztudar Angular",
+      concluido: false,
+      prioridade: 3
+   }
+];
+
+console.log(tarefas)

8cbac2937212e727cfa1ef6868f3bd3495c08f91
Jackson <porccode@gmail.com>
2026-09-06 00:38:54 -0300

    Commitando esse tarefas.controller porque tem um erro e quero encontrá-lo sozinho por agora...

--- studytask-api/src/tarefas/tarefas.controller.ts
+++ studytask-api/src/tarefas/tarefas.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { TarefasService } from './tarefas.service';
 
 @Controller('tarefas')
-export class TarefasController {}
+export class TarefasController {
+   constructor(private readonly tarefasService: TarefasService) {}
+
+   @Get()
+   listar() {
+      return this.tarefasService.listar();
+   }
+}
--- studytask-api/src/tarefas/tarefas.service.ts
+++ studytask-api/src/tarefas/tarefas.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
+type Tarefa = {
+   id: number;
+   titulo: string;
+   concluida: boolean;
+};
+
 @Injectable()
-export class TarefasService {}
+export class TarefasService {
+   private readonly tarefas: Tarefa[] = [
+      {
+         id: 1,
+         titulo: 'Estudar NestJS',
+         concluida: false,
+      },
+      {
+         id: 2,
+         titulo: 'Estudar Angular',
+         concluida: false,
+      },
+      {
+         id: 3,
+         titulo: 'Revisar TypeScript',
+         concluida: true,
+      },
+   ];
+
+   listar(): Tarefa[]{
+      return this.tarefas;
+   }
+
+}
